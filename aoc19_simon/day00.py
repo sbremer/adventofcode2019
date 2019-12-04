cad40b3c3469db7255352ee8d08f108d91ec6fab
Simon Bremer <simon.bremer@wibas-etarate.com>
2019-12-04 17:13:16 +0100

    Added template for days solution;

---
+++ aoc19_simon/day00.py
@@ -0,0 +1,12 @@
+import aoc
+
+day = 0
+lines = aoc.get_input(day)
+
+
+correct = aoc.submit(result, day)
+print(f'Answer 1 correct: {correct}')
+
+
+correct = aoc.submit(result2, day, 2)
+print(f'Answer 2 correct: {correct}')
